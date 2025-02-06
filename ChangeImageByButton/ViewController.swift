4b0a5d131eec739e5e92447b67024485ea3c7b59
Denis Raiko <raikod96@gmail.com>
2025-02-06 20:01:30 +0300

    Initial Commit

---
+++ ChangeImageByButton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChangeImageByButton
+//
+//  Created by Denis Raiko on 6.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
