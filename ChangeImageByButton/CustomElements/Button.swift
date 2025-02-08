b71cf0ea8ca5756988a09dcf23cf1c0fe89d489f
Denis Raiko <raikod96@gmail.com>
2025-02-08 13:32:15 +0300

    imageView and textLabel added

---
+++ ChangeImageByButton/CustomElements/Button.swift
@@ -0,0 +1,12 @@
+//
+//  Button.swift
+//  ChangeImageByButton
+//
+//  Created by Denis Raiko on 8.02.25.
+//
+
+import Foundation
+import UIKit
+
+class Button: UIButton {
+}
--- ChangeImageByButton/Extensions/UIView+Gradient.swift
+++ ChangeImageByButton/Extensions/UIView+Gradient.swift
@@ -13,7 +13,7 @@ extension UIView {
     func addGradient() {
         let gradient = CAGradientLayer()
         gradient.frame = bounds
-        gradient.colors = [UIColor.green.cgColor, UIColor.blue.cgColor]
+        gradient.colors = [UIColor.cyan.cgColor, UIColor.magenta.cgColor]
         gradient.opacity = 0.6
         gradient.startPoint = CGPoint(x: 0, y: 0)
         gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
